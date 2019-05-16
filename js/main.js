ccf7fa22e5ecf0b0899d089499af376742444e46
surfmuggle <1271555+surfmuggle@users.noreply.github.com>
2019-05-16 21:09:52 +0200

    Added js
    This is all

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import {pixelmatch, shout} from '../libs/pixelmatch.js';
+
+
+function callFromModule(){
+    alert("what are you doing?");
+    alert(shout("what are you doing? I am shouting!"));
+
+}
