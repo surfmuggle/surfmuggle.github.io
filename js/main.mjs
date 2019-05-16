75385c0c09ad813f0429bdbb6007ae8117bb5456
surfmuggle <1271555+surfmuggle@users.noreply.github.com>
2019-05-17 00:07:44 +0200

    Canvas and context added
    Taken from https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData

--- js/main.mjs
+++ js/main.mjs
@@ -3,8 +3,20 @@ import { shout } from '../libs/shout.mjs';
 
 
 function callFromModule(){
-    alert("what are you doing?");
-    alert(shout("what are you doing? I am shouting!"));
+ 
+    var img1 = document.getElementById("img1");
+    var img2 = document.getElementById("img2");
+    const canvas = document.getElementById("cnv3");
+    const ctx = canvas.getContext('2d');
+    ctx.rect(10, 10, 100, 100);
+    ctx.fill();
+    let imageData = ctx.getImageData(60, 60, 200, 100);
+    ctx.putImageData(imageData, 150, 10);
+           
+    // pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.1});
+    // ctx3.putImageData(diff, 0, 0);
+
+    // var numDiffPixels = pixelmatch(img1, img2, diff, 800, 600, {threshold: 0.1});
 
 }
 
