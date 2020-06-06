455763b8257488386591a1da1c4b1a9012399c3f
surfmuggle <1271555+surfmuggle@users.noreply.github.com>
2020-06-06 10:12:15 +0200

    Testset 1 - code template with comments

---
+++ cypress/sample_spec.js
@@ -0,0 +1,11 @@
+describe('Cypress Testset 1 - Startseite laden und einloggen', function(){
+    it('FDB Startseite laden', function(){})
+    // Arrange - setup initial app state
+    //              - visit a web page
+    //              - query for an element  
+    // Act - take an action
+    //              - interact with that element
+    //              
+    // Assert - make an assertion
+    //              - make an assertion about page
+})
